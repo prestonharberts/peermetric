9ca48804824f765f08db199d667f300efb539cba
Goose <tlclark1031@gmail.com>
2025-05-05 16:03:36 -0500

    fix: Registration is now functional, complete with a login

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -102,80 +102,76 @@ document.querySelector('#btnLogin').addEventListener('click', () => {
 
 // user register authentication
 // ChatGPT told me to remove the `event` within the parentheses after `'click', `
-document.querySelector('#btnRegister').addEventListener('click', () => {
-  let strErrorMessage = ""
-  let strErrorRequired = ""
+document.querySelector('#btnRegister').addEventListener('click', async () => {
 
-  let strRegisterType = document.querySelector("#txtRegisterType").value
+  let strErrorMessage = ""
+
+  // let strRegisterType = document.querySelector("#txtRegisterType").value
 
   const regEmail = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
   const regPassword = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/
-  // const regMiddleInitial = /^[a-zA-Z]$/
-  const regPhone = /^\s*(?:\+?(\d{1,3}))?([-. (]*(\d{3})[-. )]*)?((\d{3})[-. ]*(\d{2,4})(?:[-.x ]*(\d+))?)\s*$/
-  const regDiscord = /^[a-zA-Z0-9_]+#[0-9]{4}$/
 
-  let strEmail = document.querySelector("#txtEmailRegister").value
-  strEmail = strEmail.toLowerCase()
-  if (!document.querySelector("#txtEmailRegister").value) {
-    strErrorRequired += "email, "
-  } else if (!regEmail.test(strEmail)) {
-    strErrorMessage += `<p>Please enter a valid email address</p>`
-  }
-
+  let strEmail = document.querySelector("#txtEmailRegister").value.toLowerCase().trim()
   let strPassword = document.querySelector("#txtPasswordRegister").value
   let strPasswordConfirm = document.querySelector("#txtPasswordConfirm").value
-  if (!document.querySelector("#txtPasswordRegister").value) {
-    strErrorRequired += "password, "
-  } else if (!regPassword.test(strPassword)) {
-    strErrorMessage += `<p>Please enter a valid password (at least 8 characters, 1 uppercase letter, 1 lowercase letter, 1 number, and no special characters)</p>`
-  } else if (strPassword !== strPasswordConfirm) {
-    strErrorMessage += `<p>Passwords do not match</p>`
-  }
-
   let strFirstName = document.querySelector("#txtFirstName").value
-  // let strMiddleInitial = document.querySelector("#txtMiddleInitial").value
+  let strMiddleInitial = document.querySelector("#txtMiddleInitial").value
   let strLastName = document.querySelector("#txtLastName").value
-  // let strPreferredName = document.querySelector("#txtPreferredName").value
-  // if (!regMiddleInitial.test(strMiddleInitial) && document.querySelector("#txtMiddleInitial").value) {
-  //   strErrorMessage += `<p>Please enter a valid middle initial</p>`
-  // }
-  // if (strPreferredName === strFirstName || strPreferredName === strMiddleInitial || strPreferredName === strLastName) {
-  //   strPreferredName = ""
-  // }
+  let strBio = document.querySelector('#txtBio').value
 
-  let strPhone = document.querySelector("#txtPhone").value
-  if (!regPhone.test(strPhone) && document.querySelector("#txtPhone").value) {
-    strErrorMessage += `<p>Please enter a valid phone number</p>`
+  let blnError = false
+  let strErrMessage = ""
+
+  if (!regEmail.test(strEmail)) {
+    blnError = true
+    strErrorMessage += "<p>Invalid Email!</p>"
+  }
+   
+  if (!regPassword.test(strPassword)) {
+    blnError = true
+    strErrorMessage += "<p>Invalid Password (at least 8 characters, 1 uppercase letter, 1 lowercase letter, 1 number, and no special characters)</p>"
   }
 
-  let strDiscord = document.querySelector("#txtDiscord").value
-  if (!regDiscord.test(strDiscord) && document.querySelector("#txtDiscord").value) {
-    strErrorMessage += `<p>Please enter a valid discord username (username#0000)</p>`
+  if (strPassword != strPasswordConfirm)
+  {
+    blnError = true
+    strErrorMessage += "<p>Passwords do not match!</p>"
   }
 
-  if (!document.querySelector("#txtFirstName").value) {
-    strErrorRequired += "first name, "
+  if (!strBio) {
+    blnError = true
+    strErrorMessage += `<p>Please enter a Bio</p>`
   }
 
-  if (!document.querySelector("#txtLastName").value) {
-    strErrorRequired += "last name, "
+  if (!strFirstName) {
+    blnError = true
+    strErrorMessage += "<p>Invalid First Name!</p>"
   }
 
-  strErrorRequired = strErrorRequired.replace(/, $/, "")
+  if (!strLastName) {
+    blnError = true
+    strErrorMessage += "<p>Invalid Last Name!</p>"
+  }
+
+  if (strMiddleInitial.length != 1) {
+    blnError = true
+    strErrorMessage += "<p>Invalid Initial! (Must be 1 character)</p>"
+  }
+
   // ChatGPT helped me come up with this regex:
-  strErrorRequired = strErrorRequired.replace(/,(?=[^,]*$)/, ", and ")
+  // strErrorRequired = strErrorRequired.replace(/,(?=[^,]*$)/, ", and ")
 
   // ChatGPT helped me come up with this regex to remove a comma if it's the only one
-  if ((strErrorRequired.match(/,/g) || []).length === 1) {
-    strErrorRequired = strErrorRequired.replace(",", "")
-  }
+  // if ((strErrorRequired.match(/,/g) || []).length === 1) {
+  //   strErrorRequired = strErrorRequired.replace(",", "")
+  // }
 
-  if (strErrorRequired != '') {
-    strErrorRequired = "Please enter into all required fields: " + strErrorRequired
-    strErrorMessage += strErrorRequired
-  }
+  // if (strErrorRequired != '') {
+  //   strErrorRequired = "Please enter into all required fields: " + strErrorRequired
+  //   strErrorMessage += strErrorRequired
+  // }
 
-  if (strErrorMessage) {
+  if (blnError) {
     Swal.fire({
       title: 'Error!',
       html: strErrorMessage,
@@ -184,32 +180,65 @@ document.querySelector('#btnRegister').addEventListener('click', () => {
     })
   }
   else {
-    // Create a new user object
-    let newUser = {
-      email: strEmail,
-      password: strPassword,
-      firstName: strFirstName,
-      lastName: strLastName
+    localStorage.setItem('userName', strFirstName)
+
+    try
+    {
+      const objResponse = await fetch('http://localhost:1025/user', {
+        method: "POST",
+        headers: {
+          "Content-Type": "Application/JSON"
+        },
+        credentials: "include",
+        body: JSON.stringify({
+          email: strEmail,
+          password: strPassword,
+          firstName: strFirstName,
+          lastName: strLastName,
+          middleInitial: strMiddleInitial,
+          bio: strBio
+        })
+      })
+      
+      if (await objResponse.ok)
+        {
+          Login(strEmail, strPassword)
+        }
+        else
+        {
+          Swal.fire
+          ({
+            title: "Error!",
+            text: "Could not register new user!",
+            icon: 'error'
+          })
+        }
+      }
+      catch (err)
+      {
+        console.error(err.message)
+      }
     }
+})
 
-    // Add optional fields only if they are not empty
-    // if (strMiddleInitial) {
-    //   newUser.middleInitial = strMiddleInitial
-    // }
-    // if (strPreferredName) {
-    //   newUser.preferredName = strPreferredName
-    // }
-    if (strPhone) {
-      newUser.phone = strPhone
-    }
-    if (strDiscord) {
-      newUser.discord = strDiscord
-    }
-
-    users[strEmail] = newUser
-    localStorage.setItem('userName', users[strEmail].firstName)
-
-
+// Login if successful
+async function Login(strEmail, strPassword)
+{
+  // Make a new session
+  objResponse = await fetch('http://localhost:1025/session', {
+    method: "POST",
+    headers: {
+      "Content-Type": "Application/JSON"
+    },
+    body: JSON.stringify({
+      "email": strEmail,
+      "password": strPassword
+    })
+  })
+  // Change page to dashboard based on currently selected role
+  if (await objResponse.ok)
+  {
+    const strRegisterType = document.querySelector("#txtRegisterType").value
     // Redirect logic
     if (strRegisterType == 'Student account') {
       window.location.href = 'student.html'
@@ -217,8 +246,15 @@ document.querySelector('#btnRegister').addEventListener('click', () => {
       window.location.href = 'faculty.html'
     }
   }
-
-})
+  else
+  {
+    Swal.fire({
+      title: "Error!",
+      text: "Could not log in!",
+      icon: "error"
+    })
+  }
+}
 
 // jQuery
 $('#btnSwapLogin').on('click', function () {
